9f662db6114e0c25f213dc859d846675b43309b5
Gagandeep Thapar <gsthapar@gmail.com>
2023-06-15 14:52:22 -0700

    uploading constants file, WIP

--- src/astroconsts.py
+++ src/astroconsts.py
@@ -1,21 +1,23 @@
-""" 
+"""
 ASTROCONSTS
+Astronomical Constants relevant for Orbital Mechanics
 """
 
-""" 
+# pylint: disable=pointless-string-statement
+
+"""
 EARTH CONSTANTS
 """
 EARTH_MU = 398600   # [km/s2] Earth Gravitational Parameter
 EARTH_RAD = 6378    # [km] Earth Radius
 EARTH_MUSTAR = 0.01215  # [~] Earth Gravitational Parameter in Canonical Units
 
-""" 
+"""
 SUN CONSTANTS
 """
 SUN_MU = 1.3271244e11   # [km/s2] Sun gravitational parameter
 
 
-""" 
+"""
 MISC PLANETARY PARAMETERS
 """
-
