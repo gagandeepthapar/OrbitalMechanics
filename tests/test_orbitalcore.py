95c2c74cdf30dab43dad854fad8990bfeb7eb880
Gagandeep Thapar <gsthapar@gmail.com>
2023-06-20 10:28:19 -0700

    adding test cases for orbitalcore module

--- tests/test_orbitalcore.py
+++ tests/test_orbitalcore.py
@@ -92,3 +92,30 @@ class TestOrbitClass:
         sv_arr = np.array([rx, 0, 0, 0, vy, 0])
         sv_from_arr = com.StateVector.from_arr(sv_arr)
         assert basic_statevector == sv_from_arr
+        return
+
+
+class TestLambert:
+    """
+    Test Lambert Solvers
+    """
+
+    def test_universal_variable(self):
+        """
+        Test for Universal Variable Propagation
+        Test from Example 3.7 from "Orbital Mechanics for Engineering Students", Curtis
+        """
+
+        R0 = [7000, -12124, 0]
+        V0 = [2.6679, 4.6210, 0]
+        dt = 60 * 60
+
+        RF = [-3296.8, 7413.9, 0]
+        VF = [-8.2977, -0.96309, 0]
+        expect = np.array([*RF, *VF])
+
+        test = com.universal_variable_propagation(R0, V0, dt)
+
+        assert np.isclose(test, expect, rtol=1e-3).all()
+
+        return
