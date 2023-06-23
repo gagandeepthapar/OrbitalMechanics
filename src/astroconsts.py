d16586acc45f1d3278685eafdcdcd69974913af6
Gagandeep Thapar <gsthapar@gmail.com>
2023-06-23 10:23:08 -0700

    added solar pressure rad constant to astroconst file

--- src/astroconsts.py
+++ src/astroconsts.py
@@ -17,7 +17,7 @@ EARTH_ANGVEL = [0, 0, 7.2921159e-5]  # [rad/s] Angular velocity of the Earth
 SUN CONSTANTS
 """
 SUN_MU = 1.3271244e11  # [km/s2] Sun gravitational parameter
-
+SUN_PSR = 1367 / (2.998e8)  # [N/m2] Solar radiation from Sun at 1 AU
 
 """
 MISC PLANETARY PARAMETERS
