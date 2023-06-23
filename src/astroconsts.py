bdf6b2644280d25f252a11ef0ec9f3d8cbf2a7b9
Gagandeep Thapar <gsthapar@gmail.com>
2023-06-23 16:44:22 -0700

    updating constants file with Sun and AU params

--- src/astroconsts.py
+++ src/astroconsts.py
@@ -4,6 +4,13 @@ Astronomical Constants relevant for Orbital Mechanics
 """
 
 # pylint: disable=pointless-string-statement
+"""
+TEXT MODIFIERS
+"""
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RED = "\033[91m"
+DEFAULT = "\033[0m"
 
 """
 EARTH CONSTANTS
@@ -16,8 +23,9 @@ EARTH_ANGVEL = [0, 0, 7.2921159e-5]  # [rad/s] Angular velocity of the Earth
 """
 SUN CONSTANTS
 """
-SUN_MU = 1.3271244e11  # [km/s2] Sun gravitational parameter
+SUN_MU = int(1.3271244e11)  # [km/s2] Sun gravitational parameter
 SUN_PSR = 1367 / (2.998e8)  # [N/m2] Solar radiation from Sun at 1 AU
+AU = 149597870.691  # [km] conversion of 1AU to km
 
 """
 MISC PLANETARY PARAMETERS
