62a24066ce37caae6496f8adfe0d75ba249e2f3d
Gagandeep Thapar <gsthapar@gmail.com>
2023-06-22 23:52:31 -0700

    added ang vel of earth in constants file

--- src/astroconsts.py
+++ src/astroconsts.py
@@ -8,14 +8,15 @@ Astronomical Constants relevant for Orbital Mechanics
 """
 EARTH CONSTANTS
 """
-EARTH_MU = 398600   # [km/s2] Earth Gravitational Parameter
-EARTH_RAD = 6378    # [km] Earth Radius
+EARTH_MU = 398600  # [km/s2] Earth Gravitational Parameter
+EARTH_RAD = 6378  # [km] Earth Radius
 EARTH_MUSTAR = 0.01215  # [~] Earth Gravitational Parameter in Canonical Units
+EARTH_ANGVEL = [0, 0, 7.2921159e-5]  # [rad/s] Angular velocity of the Earth
 
 """
 SUN CONSTANTS
 """
-SUN_MU = 1.3271244e11   # [km/s2] Sun gravitational parameter
+SUN_MU = 1.3271244e11  # [km/s2] Sun gravitational parameter
 
 
 """
